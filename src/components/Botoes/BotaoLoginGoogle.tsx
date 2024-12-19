72c5245509051cca4dc8f33ebce563da6fb25c17
Joao Victor <joaooliverprofissional@gmail.com>
2024-12-19 15:22:09 -0300

    Mundança no botão login Google

--- src/components/Botoes/BotaoLoginGoogle.tsx
+++ src/components/Botoes/BotaoLoginGoogle.tsx
@@ -7,6 +7,6 @@ interface botaoGoogleProps{
 export default function BotaoLoginGoogle(props:botaoGoogleProps){
 
     return(
-        <button onClick={props.funcaoLogin} className=" bg-red-800 hover:bg-red-900 flex justify-center items-center gap-4 p-2 font-bold w-full">Login com google<GrGoogle fontSize={23} /> </button>   
+        <button onClick={props.funcaoLogin} className=" bg-red-800 hover:bg-red-900 flex justify-center items-center gap-4 p-2 font-bold w-full">Login com Google<GrGoogle fontSize={23} /> </button>   
      );
 }
