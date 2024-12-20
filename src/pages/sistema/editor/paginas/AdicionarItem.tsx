4166da79f4a77747fc023aa6a4b38f62f8d367de
Joao Victor <joaooliverprofissional@gmail.com>
2024-12-19 22:14:49 -0300

    Adicionando carregamento na hora de adicionar

--- src/pages/sistema/editor/paginas/AdicionarItem.tsx
+++ src/pages/sistema/editor/paginas/AdicionarItem.tsx
@@ -4,6 +4,7 @@ import { adicionaItemPorCodigo } from "../../../../functions/firestoreFunctions"
 import Item from "@/model/Item";
 import useAuthContext from "@/data/hooks/hookAuthContext";
 import Styles from '@/styles/sistema/Adicionar/adicionar.module.css'
+import Carregando from "@/components/Carregando";
 export default function AdicionarItem(){
 
     const {usuario} = useAuthContext()
@@ -11,7 +12,7 @@ export default function AdicionarItem(){
     const [quantidade, setQuantidade] = useState(0)
     const [descricao, setDescricao] = useState('')
     const [erroMensagem, setErroMensagem] = useState<string>()
-
+    const [carregando, setCarregando] = useState(false)
     function mudaQuantidade(valor:number){
         if(valor>=0){
             setQuantidade(valor)
@@ -28,8 +29,10 @@ export default function AdicionarItem(){
 
     async function adicionar(){
         if(usuario){
+            setCarregando(true)
             const resposta = await adicionaItemPorCodigo(new Item( '', nome, descricao, quantidade), usuario.codigo)
-            
+            setCarregando(false)
+
             if(resposta == false){
                 setErroMensagem('Não foi possivel adicionar.')
             }
@@ -41,6 +44,9 @@ export default function AdicionarItem(){
         }
     }
 
+    if(carregando){
+        return <Carregando/>
+    }
     return(
 
         <main className={Styles.pagina}>
--- src/pages/sistema/gestor/paginas/AdicionarItem.tsx
+++ src/pages/sistema/gestor/paginas/AdicionarItem.tsx
@@ -5,12 +5,14 @@ import Item from "@/model/Item";
 import useAuthGestorContext from "@/data/hooks/hookAuthGestorContext";
 import Styles from '@/styles/sistema/Adicionar/adicionar.module.css'
 import IconeAdicionarEstoque from "../../components/Icones/IconeAdicionarEstoque";
+import Carregando from "@/components/Carregando";
 export default function AdicionarItem(){
 
     const {usuarioGestor} = useAuthGestorContext()
     const [nome, setNome] = useState<string>('')
     const [quantidade, setQuantidade] = useState(0)
     const [descricao, setDescricao] = useState('')
+    const [carregando, setCarregando] = useState(false)
     function mudaQuantidade(valor:number){
         if(valor>=0){
             setQuantidade(valor)
@@ -19,14 +21,22 @@ export default function AdicionarItem(){
         }
     }
     
-    function adicionar(){
+    async function adicionar(){
         if(usuarioGestor){
-            adicionaItem(usuarioGestor.uid, new Item( '', nome, descricao, quantidade))
+            setCarregando(true)
+            await adicionaItem(usuarioGestor.uid, new Item( '', nome, descricao, quantidade))
+            setDescricao('')
+            setNome('')
+            setQuantidade(0)
+            setCarregando(false)
+
         }else{
             console.log('Erro')
         }
     }
-
+    if(carregando){
+        return <Carregando/>
+    }
     return(
         <main className={Styles.pagina}>
             <div className={Styles.formulario}>
