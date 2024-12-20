b54db51ffe90c2cded5e1efe42d72b3fd84aea7e
Joao Victor <joaooliverprofissional@gmail.com>
2024-12-19 22:37:29 -0300

    Adição do código na página de espera

--- src/pages/sistema/PaginaDeEsperaGestor.tsx
+++ src/pages/sistema/PaginaDeEsperaGestor.tsx
@@ -2,10 +2,10 @@ import useAuthContext from "@/data/hooks/hookAuthContext"
 import Styles from '@/styles/PaginaDeEsperaGestor.module.css'
 
 export default function PaginaDeEsperaGestor(){
-    const {cleanUsuario}= useAuthContext()
+    const {cleanUsuario, usuario}= useAuthContext()
     return(
     <div className={Styles.pagina}>
-        <p className={Styles.titulo}>Espere seu gestor aceitar você</p>
+        <p className={Styles.titulo}>Espere seu gestor de código {usuario?.codigo} aceitar você</p>
         <button onClick={()=>{cleanUsuario()}} className={Styles.botao}> Sair</button>
     </div>)
 }
